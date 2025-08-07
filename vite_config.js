30afa155683bc0563c771a74b7f4784f6f42e595
Krazay17 <Joshmass7@gmail.com>
2025-08-07 16:22:22 -0400

    second commit

--- vite_config.js
+++ vite_config.js
@@ -4,7 +4,7 @@ export default {
 	root: '.',               // Project root
 	publicDir: 'public',     // Static files
 	build: {
-    outDir: 'dist',        // Output folder for deployment
-    emptyOutDir: true
-  }
+		outDir: 'dist',        // Output folder for deployment
+		emptyOutDir: true
+	}
 };
