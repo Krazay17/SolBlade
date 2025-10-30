ef57b1c3fd535574622c912f93aa23f0273bc894
Krazay17 <Joshmass7@gmail.com>
2025-10-30 15:14:46 -0400

    shared folder import test

--- client/src/scenes/World1.js
+++ client/src/scenes/World1.js
@@ -1,8 +1,10 @@
+import { sharedTest } from "../../../shared/Utils";
 import World from "./World";
 
 export default class World1 extends World {
     constructor(game) {
         super(game, 'world1');
+        sharedTest();
     }
     get spawnPos() { return this.getRespawnPoint() }
 }
